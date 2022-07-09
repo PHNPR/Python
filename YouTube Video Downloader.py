2c121ffcde6ee022f1c7f2b88ca02b69358c125c
Praneeth Reddy <104026937+PHNPR@users.noreply.github.com>
2022-07-09 11:25:59 +0530

    updated comments

--- YouTube Video Downloader.py
+++ YouTube Video Downloader.py
@@ -1,7 +1,9 @@
 # You need to install pytube library to run this code.
-# Use 'pip install pytube' command in cmd to fulfill the requirements.
+# Run 'pip install pytube' command in cmd to install pytube package.
+
 # If you open a folder while running the code , the videos is downloaded to that certain folder.
 # Else the video gets saved in localdisk/users/your_pc_name.
+
 import pytube as pt
 link = input('Enter URL:')
 yt = pt.YouTube(link)
